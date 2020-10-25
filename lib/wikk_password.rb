e4346b5c996f3bf92305ed842db6518df689f3c0
robertburrowes <r.burrowes@auckland.ac.nz>
2020-10-25 21:47:30 +1300

    Tidy up yard comments

--- lib/wikk_password.rb
+++ lib/wikk_password.rb
@@ -4,7 +4,8 @@ module WIKK
   require 'unix_crypt'
   require 'base64'
   
-  #READS/WRITES our private password file entries.
+  # READS/WRITES our private password file entries.
+  #
   # @attr_reader [String] user the decrypted text
   # @attr_reader [String] password the encrypted password, in form $type$initial_vector$encrypted_text
   class Password
@@ -12,12 +13,13 @@ module WIKK
 
     attr_reader :user, :password
     
-    #New. Fetches a user entry from the password file, or creates a new user (call via Passwd::add_user)
-    #  @param user [String] User name to fetch from password file, or to create, if new_user == true
-    #  @param config [WIKK:Configuration] or hash or class with attr_readers :passwordFile, :encryption, :key
-    #  @param new_user [Boolean] If true, then the user shouldn't be in password file.
-    #  @return [WIKK::Password]
-    #  @raise [IndexError] if the user entry exists.
+    # New. Fetches a user entry from the password file, or creates a new user (call via Passwd::add_user)
+    #
+    # @param user [String] User name to fetch from password file, or to create, if new_user == true
+    # @param config [WIKK:Configuration] or hash or class with attr_readers :passwordFile, :encryption, :key
+    # @param new_user [Boolean] If true, then the user shouldn't be in password file.
+    # @return [WIKK::Password]
+    # @raise [IndexError] if the user entry exists.
   	def initialize(user, config, new_user=false)
       if config.class == Hash
         sym = config.each_with_object({}) { |(k,v),h| h[k.to_sym] = v }
@@ -28,39 +30,43 @@ module WIKK
   	  raise IndexError, "User \"#{user}\" not found" if getpwnam(user) == false && !new_user
   	end
   	
-  	#Sets the user password, but does not save this. You must call save().
-  	#  @param password [String] the clear text password to encypt
-  	#  @return [String] the password file password entry.
+  	# Sets the user password, but does not save this. You must call save().
+    #
+  	# @param password [String] the clear text password to encypt
+  	# @return [String] the password file password entry.
     def set_password(password) 
       @password = encrypt(password, @config.encryption)
     end
     
-    #Compare an SHA256 hashed password + challenge with this users password
-    #  @param challenge [String] a random string, sent to the remote client, added to the password, and SHA256 hashed
-    #  @param response [String] the remote clients hex_SHA256(password + challenge)
-    #  @return [Boolean] True if the users password matches the one that created the response.
-    #  @note The password entry must be decryptable, not a UNIX style hash.
-    #  @raise [ArgumentError] if the encryption method is unknown.
+    # Compare an SHA256 hashed password + challenge with this users password
+    #
+    # @param challenge [String] a random string, sent to the remote client, added to the password, and SHA256 hashed
+    # @param response [String] the remote clients hex_SHA256(password + challenge)
+    # @return [Boolean] True if the users password matches the one that created the response.
+    # @note The password entry must be decryptable, not a UNIX style hash.
+    # @raise [ArgumentError] if the encryption method is unknown.
     def valid_sha256_response?(challenge, response)
       return response == Digest::SHA256.digest(decrypt + challenge).unpack('H*')[0]
     end
     
-    #Compare an SHA256 hashed password + challenge with this users password
-    #  @param user [String] User name to fetch from password file, or to create, if new_user == true
-    #  @param config [WIKK:Configuration] or hash or class with attr_readers :passwordFile, :encryption, :key
-    #  @param challenge [String] a random string, sent to the remote client, added to the password, and SHA256 hashed
-    #  @param response [String] the remote clients hex_SHA256(password + challenge)
-    #  @return [Boolean] True if the users password matches the one that created the response.
-    #  @note The password entry must be decryptable, not a UNIX style hash.
-    #  @raise [ArgumentError] if the encryption method is unknown.
+    # Compare an SHA256 hashed password + challenge with this users password
+    #
+    # @param user [String] User name to fetch from password file, or to create, if new_user == true
+    # @param config [WIKK:Configuration] or hash or class with attr_readers :passwordFile, :encryption, :key
+    # @param challenge [String] a random string, sent to the remote client, added to the password, and SHA256 hashed
+    # @param response [String] the remote clients hex_SHA256(password + challenge)
+    # @return [Boolean] True if the users password matches the one that created the response.
+    # @note The password entry must be decryptable, not a UNIX style hash.
+    # @raise [ArgumentError] if the encryption method is unknown.
     def self.valid_sha256_response?(user, config, challenge, response)
       self.new(user, config).valid_sha256_response?(challenge, response)
     end
       
-    #Compares the password with the user's password by encrypting the password passed in
-    #  @param password [String] The clear text password
-    #  @return [Boolean] True if the passwords match
-    #  @raise [ArgumentError] if the encryption method is unknown.
+    # Compares the password with the user's password by encrypting the password passed in
+    #
+    # @param password [String] The clear text password
+    # @return [Boolean] True if the passwords match
+    # @raise [ArgumentError] if the encryption method is unknown.
     def valid?(ct_password)
       ignore,encryption,iv,password = @password.split('$')
       encryption = 'DES' if ignore != '' #No $'s in DES password, so ignore has text.
@@ -73,12 +79,13 @@ module WIKK
       end
     end
     
-    #Adds a user to the password file
-    #  @param user [String] New user name. Raises an error if the user exists
-    #  @param password [String] Clear text password. Raises an error if this is nil or ''
-    #  @note Modifies the password file.
-    #  @raise [IndexError] if the user entry exists.
-    #  @raise [ArgumentError] if the password is nil or empty.
+    # Adds a user to the password file
+    #
+    # @param user [String] New user name. Raises an error if the user exists
+    # @param password [String] Clear text password. Raises an error if this is nil or ''
+    # @note Modifies the password file.
+    # @raise [IndexError] if the user entry exists.
+    # @raise [ArgumentError] if the password is nil or empty.
     def self.add_user(user,password,config)
       user_record = self.new(user, config, true)
       raise IndexError, "User \"#{user}\" is already present"  if user_record.password != nil
@@ -87,24 +94,27 @@ module WIKK
       user_record.save
     end
         
-    #Saves changes or a new user entry into the password file
+    # Saves changes or a new user entry into the password file
+    #
     def save
       loadfile
       @pwent[@user] = @password
       writefile
     end
     
-    #Outputs password file entry as a string
-    #  @return [String] password file entry.
+    # Outputs password file entry as a string
+    #
+    # @return [String] password file entry.
     def to_s
       "#{@user}:#{@password}"
     end
 
   	private
   	
-  	#Fetch a password file entry by user's name
-  	#  @param user [String] user name
-  	#  @return [Boolean] True if user entry exists
+  	# Fetch a password file entry by user's name
+    #
+  	# @param user [String] user name
+  	# @return [Boolean] True if user entry exists
   	def getpwnam(user)
   	  loadfile
   	  @user = user
@@ -112,7 +122,8 @@ module WIKK
   	  return @password != nil #i.e. Found a user entry
 	  end
 	  
-	  #Read the password file into the @pwent hash
+	  # Read the password file into the @pwent hash
+    #
   	def loadfile
   	  @pwent = {}
   	  File.open(@config.passwordFile, "r") do |fd|
@@ -123,7 +134,8 @@ module WIKK
       end
     end
     
-	  #Overwrite the password file from the @pwent hash
+	  # Overwrite the password file from the @pwent hash
+    #
   	def writefile
   	  File.open(@config.passwordFile, "w+") do |fd|
   	    @pwent.each do |k,v|
@@ -132,10 +144,11 @@ module WIKK
       end
     end
     
-  	#Encrypts a clear text password
-  	#  @param password [String] The clear text password
-  	#  @param challenge [String] Norm
-    #  @raise [ArgumentError] if the encryption algorithm isn't known.
+  	# Encrypts a clear text password
+    #
+  	# @param password [String] The clear text password
+  	# @param challenge [String] Norm
+    # @raise [ArgumentError] if the encryption algorithm isn't known.
     def encrypt(password,  algorithm = "aes256", pwd_iv = nil)
       case algorithm
       when "aes256"
@@ -156,9 +169,10 @@ module WIKK
       end
     end
     
-    #Decrypts @password, if this is possible
-    #  @return [String] the clear text password
-    #  @raise [ArgumentError] if the encryption type can't be decrypted.
+    # Decrypts @password, if this is possible
+    #
+    # @return [String] the clear text password
+    # @raise [ArgumentError] if the encryption type can't be decrypted.
     def decrypt
       ignore,encryption,iv,password = @password.split('$')
       case encryption
